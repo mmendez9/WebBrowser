eaaff17639c7ef2eddb60b5d4322de26b9a6f639
Mayra Mendez <mmendez9@dtcc.edu>
2016-09-06 16:44:35 -0700

    Details added

--- WebBrowser/Form1.Designer.cs
+++ WebBrowser/Form1.Designer.cs
@@ -31,11 +31,14 @@
             this.urlTextBox = new System.Windows.Forms.TextBox();
             this.goButton = new System.Windows.Forms.Button();
             this.WebBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // urlTextBox
             // 
-            this.urlTextBox.Location = new System.Drawing.Point(12, 229);
+            this.urlTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.urlTextBox.Location = new System.Drawing.Point(3, 3);
             this.urlTextBox.Name = "urlTextBox";
             this.urlTextBox.Size = new System.Drawing.Size(196, 20);
             this.urlTextBox.TabIndex = 0;
@@ -43,7 +46,8 @@
             // 
             // goButton
             // 
-            this.goButton.Location = new System.Drawing.Point(214, 229);
+            this.goButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.goButton.Location = new System.Drawing.Point(205, 3);
             this.goButton.Name = "goButton";
             this.goButton.Size = new System.Drawing.Size(58, 20);
             this.goButton.TabIndex = 1;
@@ -53,25 +57,36 @@
             // 
             // WebBrowser1
             // 
-            this.WebBrowser1.Location = new System.Drawing.Point(9, 8);
+            this.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WebBrowser1.Location = new System.Drawing.Point(0, 0);
             this.WebBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.WebBrowser1.Name = "WebBrowser1";
-            this.WebBrowser1.Size = new System.Drawing.Size(262, 211);
+            this.WebBrowser1.Size = new System.Drawing.Size(484, 461);
             this.WebBrowser1.TabIndex = 2;
             this.WebBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.urlTextBox);
+            this.flowLayoutPanel1.Controls.Add(this.goButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 435);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(484, 26);
+            this.flowLayoutPanel1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.WebBrowser1);
-            this.Controls.Add(this.goButton);
-            this.Controls.Add(this.urlTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -80,6 +95,7 @@
         private System.Windows.Forms.TextBox urlTextBox;
         private System.Windows.Forms.Button goButton;
         private System.Windows.Forms.WebBrowser WebBrowser1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
